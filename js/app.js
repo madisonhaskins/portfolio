e39ec62723278a4c252c28771985e99f545eb838
Madison <madisonhaskinsemail@gmail.com>
2019-08-19 22:09:03 -0500

    fix portfolio grid

--- js/app.js
+++ js/app.js
@@ -1,64 +1,4 @@
 
-$(document).foundation({
-  	orbit: {
-		animation: 'fade', // Sets the type of animation used for transitioning between slides, can also be 'fade'
-		timer_speed: 5000, // Sets the amount of time in milliseconds before transitioning a slide
-		pause_on_hover: false, // Pauses on the current slide while hovering
-		resume_on_mouseout: false, // If pause on hover is set to true, this setting resumes playback after mousing out of slide
-		animation_speed: 500, // Sets the amount of time in milliseconds the transition between slides will last
-		slide_number: false,
-		navigation_arrows: true,
-		swipe: true
-  	}
-});
-
-// Headroom
-
-var navigationHeadroom = (function() {
-
-    var docElem = document.documentElement,
-        nav = document.querySelector("#navigation"),
-        didScroll = false,
-        changeHeaderOn = 1;
-
-	var headroom = new Headroom(nav, {
-	  	"tolerance": 0,
-	  	"offset": 600,
-	  	"classes": {
-	    	"initial": "headroom",
-	    	"pinned": "pinned",
-	    	"unpinned": "unpinned"
-	  	}
-	});
-
-    function init() {
-        window.addEventListener("scroll", function(event) {
-            if( !didScroll ) {
-                didScroll = true;
-                setTimeout(scrollPage, 0);
-            }
-        }, false);
-    }
-
-    function scrollPage() {
-        var sy = scrollY();
-        if ( sy >= changeHeaderOn ) {
-        	headroom.init();
-        } else {
-        	headroom.destroy();
-        }
-        didScroll = false;
-    }
-
-    function scrollY() {
-        return window.pageYOffset || docElem.scrollTop;
-    }
-
-    init();
-
-})();
-
-
 // Open Project
 
 $('.project-item').click(function () {
@@ -71,7 +11,7 @@ $('.project-item').click(function () {
 
     $('html, body').animate({
         scrollTop: $(projectID).offset().top - 76
-    }, 600);
+    }, 1);
 
 });
 
@@ -81,8 +21,7 @@ $('.project-close').click(function () {
 
     $('html, body').animate({
         scrollTop: $('#mywork').offset().top - 76
-    }, 600);
-
+    }, 1);
 });
 
 
